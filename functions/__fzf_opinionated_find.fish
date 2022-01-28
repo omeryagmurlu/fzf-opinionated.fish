24ff11c776ca29fa00a2dfb7e09f59da627ee033
Ömer Erdinç Yağmurlu <omeryagmurlu@gmail.com>
2022-01-28 12:59:41 +0100

    symlink support

--- functions/__fzf_opinionated_find.fish
+++ functions/__fzf_opinionated_find.fish
@@ -3,7 +3,7 @@ function __fzf_opinionated_find
     set --local --export SHELL (which fish)
     set --local selected (
         fd --hidden --follow --color=always --exclude=.git 2> /dev/null |
-        fzf --ansi --prompt=❯❯❯ --preview='__fzf_opinionated_previewer {}' --preview-window right:60%:noborder --height 50% --bind='ctrl-p:toggle-preview'
+        fzf --ansi --prompt=❯❯❯ --preview='__fzf_opinionated_previewer {}' --preview-window right:60%:noborder --height 100% --bind='ctrl-p:toggle-preview'
     )
 
     if test $status -eq 0
--- functions/__fzf_opinionated_previewer.fish
+++ functions/__fzf_opinionated_previewer.fish
@@ -1,25 +1,29 @@
 function __fzf_opinionated_previewer
-	for FIFIFI in $argv 
+	for FIFIFI in $argv
 		if test -d $FIFIFI
-			echo -n "<DIR>    "
-		else if string match -r -q "binary" (file --mime $FIFIFI)
-			echo -n "<BINARY> "
-		else
-			echo -n "<FILE>   "
-		end
+			echo -n "<DIR>        "
+			du -sh $FIFIFI
 
-		du -sh $FIFIFI
-
-		if test -d $FIFIFI
 			ls -Axh1 --si --color=always $FIFIFI
+		else if string match -r -q "symlink" (file --mime $FIFIFI)
+			echo -n "<SYMLINK>    "
+			du -sh $FIFIFI
+
+			__fzf_opinionated_previewer (readlink -f $FIFIFI)
 		else if string match -r -q "binary" (file --mime $FIFIFI)
+			echo -n "<BINARY>     "
+			du -sh $FIFIFI
+
 			hexdump -C -n 4096 $FIFIFI
 			if test (du -sb $FIFIFI | cut -f1) -gt 4096
 				echo "only first 4096 bytes printed"
 			end
 		else
+			echo -n "<FILE>       "
+			du -sh $FIFIFI
+
 			bat --style=numbers --color=always --wrap=auto \
-		 		--terminal-width=(tput cols) \
+				--terminal-width=(tput cols) \
 				$FIFIFI
 		end
 	end
